204b25b74f5fef605cb00ccf7e72f6da5ae5b872
Dimitris Tsesmelis <zas1122@hotmail.com>
2021-03-13 16:25:21 +0200

    Add shards rebalancing and separate storage volume of coordinator and worker

--- KubernetesDeployment/scripts/startK8s.sh
+++ KubernetesDeployment/scripts/startK8s.sh
@@ -2,7 +2,8 @@
 PATHTOFILES="/home/azureuser/MobilityDB-in-Azure/KubernetesDeployment"
 sudo kubectl create secret generic postgresql-secrets --from-file=$PATHTOFILES/secrets/
 sudo kubectl create -f $PATHTOFILES/postgres-secrets-params.yaml
-sudo kubectl create -f $PATHTOFILES/postgres-storage.yaml 
+sudo kubectl create -f $PATHTOFILES/postgres-storage-worker.yaml
+sudo kubectl create -f $PATHTOFILES/postgres-storage-coordinatoryaml  
 sudo kubectl create -f $PATHTOFILES/postgres-deployment.yaml 
 sudo kubectl create -f $PATHTOFILES/postgres-service.yaml
 sudo kubectl create -f $PATHTOFILES/postgres-deployment-workers.yaml
