23d3341033dd6c2d9b4e439ff5aa1f0e6d137efa
Dimitris Tsesmelis <zas1122@hotmail.com>
2021-03-15 17:14:43 +0200

    assign Citus coordinator to K8S master node

--- automaticClusterDeployment/KubernetesCluster/runOnMaster2.sh
+++ automaticClusterDeployment/KubernetesCluster/runOnMaster2.sh
@@ -8,3 +8,8 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml
+
+#By default, the cluster will not schedule pods on the control-plane node for security reasons. 
+#If you want to be able to schedule pods on the control-plane node, run:
+kubectl label nodes coordinator dedicated=master
+kubectl taint nodes --all node-role.kubernetes.io/master-
